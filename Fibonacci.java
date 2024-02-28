e636759ec9836e7153cd729e1d626a6224b04f28
Bo1dos <96138962+Bo1dos@users.noreply.github.com>
2024-02-28 12:42:33 +0300

    Add files via upload

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,13 +1,13 @@
 import java.math.BigInteger;
 
 public class Fibonacci {
-    public static BigInteger nthNumFibonacci(int n) {
+    public static BigInteger nthNumFibonacci(long n) {
 
         BigInteger fibonacci;
         BigInteger num1 = new BigInteger("0");
         BigInteger num2 = new BigInteger("1");
 
-        for(int loop = 0; loop < n; loop++) {
+        for(long loop = 0; loop < n; loop++) {
             fibonacci = num1.add(num2);
             num1 = num2;
             num2 = fibonacci;
