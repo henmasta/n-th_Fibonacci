b63e3576f22270e139b779f9af664a9a4f91b069
Bo1dos <96138962+Bo1dos@users.noreply.github.com>
2024-03-01 13:37:05 +0300

    Updating files from the main branch

--- Fibonacci.java
+++ Fibonacci.java
@@ -7,7 +7,7 @@ public class Fibonacci {
         BigInteger num1 = new BigInteger("0");
         BigInteger num2 = new BigInteger("1");
 
-        for(long loop = 0; loop < n; loop++) {
+        for(int loop = 0; loop < n; loop++) {
             fibonacci = num1.add(num2);
             num1 = num2;
             num2 = fibonacci;
